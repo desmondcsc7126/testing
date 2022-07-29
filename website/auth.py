f09bfb4deb573f7a7c716e75887ed347d1317d62
desmondcsc7126 <101800952+desmondcsc7126@users.noreply.github.com>
2022-07-29 14:49:30 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -40,7 +40,7 @@ bucketname = 'desmondchongsoonchuen'
 
 @auth.route('/registration',methods = ['POST', 'GET'])
 def register():
-    return render_template('registration.html', myData = "2nd")
+    return render_template('registration.html', myData = "1st")
 
 @auth.route('/registerUser',methods = ['POST','GET'])
 def registerUser():
